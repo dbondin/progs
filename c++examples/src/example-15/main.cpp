0a5fd3634316374ce0d5ab218c845fd6021bfc89
Dmitry Bondin <dbondin@ya.ru>
2014-09-29 10:47:14 +0400

    [c++examples] add example-15 (strings)

---
+++ c++examples/src/example-15/main.cpp
@@ -0,0 +1,111 @@
+#include <string>
+#include <iostream>
+#include <stdexcept>
+#include <sstream>
+
+#include <stdio.h>
+
+using namespace std;
+
+int main(int argc, char **argv) {
+//	string s;
+//	cout<<s<<" "<<s.length()<<" "<<s.capacity()<<endl;
+
+//	string s(10, 'A');
+//	cout<<s<<" "<<s.length()<<" "<<s.capacity()<<endl;
+
+//	string s(10, 0);
+//	cout<<s.length()<<" "<<s.capacity()<<endl;
+
+//	string s("hello");
+//	cout<<s<<" "<<s.length()<<" "<<s.capacity()<<endl;
+
+//	string s;
+//
+//	s = "hello";
+//	cout<<s<<" "<<s.length()<<" "<<s.capacity()<<endl;
+//
+//	puts(s.c_str());
+//	puts(s.data());
+
+//	string s;
+//	s.reserve(10);
+//	for(int i=0; i<11; i++) {
+//		s += 'x';
+//		cout<<s<<" "<<s.length()<<" "<<s.capacity()<<endl;
+//		printf("%p\n", s.c_str());
+//	}
+
+//	string s("hello");
+//	cout<<s[0]<<endl;
+//	cout<<s.at(0)<<endl;
+//	cout<<s[10]<<endl;
+//	try {
+//		cout<<s.at(10)<<endl;
+//	}
+//	catch(std::out_of_range & ex) {
+//		cout<<"exception "<<ex.what()<<endl;
+//	}
+//	cout<<":)"<<endl;
+//	s[0] = 'H';
+//	cout<<s<<" "<<s.length()<<" "<<s.capacity()<<endl;
+//	s.at(0) = 'h';
+//	cout<<s<<" "<<s.length()<<" "<<s.capacity()<<endl;
+
+//	string s1("hello");
+//	string s2("help");
+//
+//	cout<<(s1<s2)<<endl;
+//	cout<<s1.compare("hello")<<endl;
+
+//	string s("hello");
+//	cout<<s<<" "<<s.length()<<" "<<s.capacity()<<endl;
+//	s = "123";
+//	cout<<s<<" "<<s.length()<<" "<<s.capacity()<<endl;
+//	s.assign("45678899");
+//	cout<<s<<" "<<s.length()<<" "<<s.capacity()<<endl;
+
+//	string s1("hello");
+//	string s2("world");
+//	cout<<s1<<" "<<s2<<endl;
+//	s1.swap(s2);
+//	cout<<s1<<" "<<s2<<endl;
+//	std::swap(s1, s2);
+//	cout<<s1<<" "<<s2<<endl;
+
+//	string s;
+//	s.append("hello");
+//	s.append(5, ' ');
+//	s.append(s);
+//	s.push_back('X');
+//	cout<<s<<" "<<s.length()<<" "<<s.capacity()<<endl;
+
+//	string s("hellohellohellohello");
+//	printf("%p: %s\n", s.c_str(), s.c_str());
+//	string s1 = s.substr();
+//	printf("%p: %s\n", s.c_str(), s.c_str());
+//	printf("%p: %s\n", s1.c_str(), s1.c_str());
+//	string s2 = s.substr(10);
+//	printf("%p: %s\n", s.c_str(), s.c_str());
+//	printf("%p: %s\n", s2.c_str(), s2.c_str());
+
+//	while(true) {
+//		string s;
+//		cin>>s;
+//		cout<<s<<" "<<s.length()<<endl;
+//	}
+
+//	string s;
+//	getline(cin, s);
+//	cout<<s<<" "<<s.length()<<" "<<s.capacity()<<endl;
+//	stringstream ss(s);
+//	while(ss.good()) {
+//		string s1;
+//		ss>>s1;
+//		cout<<s1<<" "<<s1.length()<<" "<<s1.capacity()<<endl;
+//	}
+
+
+
+	return 0;
+}
