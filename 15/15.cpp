9e30fedc577da8fb430b3ac2493534d9b7ae4df5
Dmitry Bondin <dbondin@ya.ru>
2015-05-31 16:42:19 +0300

    [15] Working 3x3

--- 15/15.cpp
+++ 15/15.cpp
@@ -3,12 +3,16 @@
 #include <cstring>
 #include <cmath>
 #include <unistd.h>
+#include <stdlib.h>
+#include <time.h>
 
-#if 1
-#define DPRINT(x) { cout << "DPRINT: " << (x) << endl; }
-#else
-#define DPRINT(x) { }
-#endif
+#ifdef DEBUG
+#define DPRINT( x ) { cout << "DPRINT: " << (x) << endl; }
+#define DPRINT2( x, y ) { cout << "DPRINT: " << (x) << " " << (y) << endl; }
+#else /* DEBUG */
+#define DPRINT( x ) { }
+#define DPRINT2( x, y ) { }
+#endif /* DEBUG */
 
 #define WIDTH 3
 #define HEIGHT 3
@@ -27,13 +31,41 @@ struct PosList {
 	unsigned long int size;
 };
 
+void initSolutions();
+bool check(Pos & pos);
+void swap(Pos & pos, int index1, int index2);
+void markExist(Pos & pos);
+void addPos(Pos & pos);
+void moveLeft(int index);
+void moveRight(int index);
+void moveUp(int index);
+void moveDown(int index);
+int doit(int index);
+void printPos(Pos & pos);
+unsigned long int factorial(unsigned long int value);
+void makeRandomPos(Pos & pos);
+
 PosList posList;
 unsigned char * exist;
-
-Pos sol1 = {{1, 2, 3, 4, 5, 6, 7, 8, 0}, 8, -1};
-Pos sol2 = {{1, 2, 3, 4, 5, 6, 8, 7, 0}, 8, -1};
-
 vector<Pos> solutions;
+unsigned long int posListMax = factorial(NUM) / 2;
+unsigned long long int existMax = (unsigned long long int) pow(NUM, NUM);
+
+void makeRandomPos(Pos & pos) {
+	srand(time(NULL));
+	for(int i=0; i<NUM; i++) {
+		pos.data[i] = i;
+	}
+	for(int i=0; i<NUM; i++) {
+		swap(pos, i, rand() % NUM);
+	}
+	for(int i=0; i<NUM; i++) {
+		if(pos.data[i] == 0) {
+			pos.zeroPos = i;
+		}
+	}
+	pos.parent = -1;
+}
 
 void initSolutions() {
 	Pos sol1;
@@ -56,6 +88,8 @@ void initSolutions() {
 			sol2.data[i] = i + 1;
 		}
 	}
+	sol1.data[NUM - 1] = 0;
+	sol2.data[NUM - 1] = 0;
 
 	solutions.push_back(sol1);
 	solutions.push_back(sol2);
@@ -63,11 +97,11 @@ void initSolutions() {
 
 bool check(Pos & pos) {
 	for(vector<Pos>::iterator iter = solutions.begin(); iter != solutions.end(); iter ++) {
-		if(memcmp(pos.data, (*iter).data, sizeof(pos.data)) != 0) {
-			return false;
+		if(memcmp(pos.data, (*iter).data, sizeof(pos.data)) == 0) {
+			return true;
 		}
 	}
-	return true;
+	return false;
 }
 
 void swap(Pos & pos, int index1, int index2) {
@@ -89,10 +123,15 @@ void markExist(Pos & pos) {
 
 bool isExist(Pos & pos) {
 
-	int existIndex = 0;
+	unsigned long long int existIndex = 0;
 	for(int i=0; i<NUM; i++) {
 		existIndex *= NUM;
 		existIndex += pos.data[i];
+	}
+
+	if(existIndex >= existMax) {
+		cerr<<"existMax overfull ("<<existIndex<<"/"<<existMax<<")"<<endl;
+		return true;
 	}
 
 	return exist[existIndex];
@@ -100,9 +139,14 @@ bool isExist(Pos & pos) {
 
 void addPos(Pos & pos) {
 	if(!isExist(pos)) {
+		if(posList.size >= posListMax) {
+			cerr<<"posListMax overfill !!!"<<endl;
+			return;
+		}
 		posList.list[posList.size] = pos;
 		posList.size ++;
 		markExist(pos);
+		DPRINT2("size =", posList.size);
 	}
 }
 
@@ -120,9 +164,7 @@ void moveLeft(int index) {
 
 void moveRight(int index) {
 	Pos pos = posList.list[index];
-	DPRINT("move right 1");
 	if((pos.zeroPos % WIDTH) != (WIDTH - 1)) {
-		DPRINT("move right 2");
 		Pos newPos;
 		newPos.parent = index;
 		newPos.zeroPos = pos.zeroPos + 1;
@@ -157,32 +199,36 @@ void moveDown(int index) {
 }
 
 int doit(int index) {
-
-	DPRINT("doit 1");
-	DPRINT(index);
-	for(int i=index; i < posList.size; i++)
+	unsigned long int size = posList.size;
+	DPRINT2("doit", index);
+	for(int i=index; i < size; i++)
 	{
-		DPRINT("doit 3");
-		Pos pos = posList.list[i];
-		if(check(pos)) {
+		if(check(posList.list[i])) {
 			DPRINT("doit found !!!");
 			return i;
 		}
 	}
-	DPRINT("doit 2");
-	for(int i=index; i < posList.size; i++)
+	for(int i=index; i < size; i++)
 	{
 		moveUp(i);
 		moveRight(i);
 		moveDown(i);
 		moveLeft(i);
 	}
-
-	return doit(posList.size);
+	if(size == posList.size) {
+		return -1;
+	}
+      	return doit(size);
 }
 
 void printPos(Pos & pos) {
 	for(int i=0; i<HEIGHT; i++) {
+		if(i == 0) {
+			cout<<"/ ";
+		}
+		else {
+			cout<<"  ";
+		}
 		for(int j=0; j<WIDTH; j++) {
 			if(pos.data[i * WIDTH + j] == 0) {
 				cout<<"_ ";
@@ -195,8 +241,6 @@ void printPos(Pos & pos) {
 	}
 }
 
-Pos pos0 = {{ 5, 4, 3, 2, 1, 0}, 5, -1};
-
 unsigned long int factorial(unsigned long int value) {
 	if(value < 2) {
 		return 1;
@@ -205,25 +249,39 @@ unsigned long int factorial(unsigned long int value) {
 }
 
 int main(int argc, char **argv) {
-	exist = new unsigned char [(int)pow(NUM, NUM)];
-	posList.list = new Pos[factorial(NUM) / 2];
+	exist = new unsigned char [existMax];
+	posList.list = new Pos[posListMax];
 	posList.size = 0;
 	initSolutions();
-	cout<<"MAX LIST SIZE = "<<(factorial(NUM) / 2)<<endl;
+	Pos p0;
+	makeRandomPos(p0);
+	cout<<"Initial position:"<<endl;
+	printPos(p0);
+	cout<<"Final positions:"<<endl;
 	printPos(solutions[0]);
 	printPos(solutions[1]);
-	addPos(pos0);
+	cout<<"Exist max size: "<<existMax<<endl;
+	cout<<"PosList max size: "<<posListMax<<endl;
+	addPos(p0);
 	int index = doit(0);
-	vector<int> st;
-	while(index != -1) {
-		st.push_back(index);
-		index = posList.list[index].parent;
+	if(index == -1) {
+		cout<<"No solution found. "<<posList.size<<" combinations checked. Exiting."<<endl;
+	}
+	else {
+		vector<int> st;
+		while(index != -1) {
+			st.push_back(index);
+			index = posList.list[index].parent;
+		}
+
+		while(!st.empty()) {
+			int index = st.back();
+			st.pop_back();
+			cout<<"index = "<<index<<endl;
+			printPos(posList.list[index]);
+		}
 	}
 
-	while(!st.empty()) {
-		int index = st.back();
-		st.pop_back();
-		cout<<"index ="<<index<<endl;
-		printPos(posList.list[index]);
-	}
+	delete [] posList.list;
+	delete [] exist;
 }
